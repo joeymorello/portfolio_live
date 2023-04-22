9f7386b48bf7e7f7eb918cc0e32a7b347ed98c3e
Joey Morello <brian@m1interactive.com>
2023-04-22 13:49:40 -0500

    first commit

---
+++ js/projectPage.js
@@ -0,0 +1,23 @@
+const homeButton = document.getElementById('home_btn')
+let homeButtonInterval
+const min = 1
+const max = 8
+let homeButtonImgIndex = Math.floor(min + Math.random()*(max - min + 1))
+
+homeButton.src = `../../assets/home_icons/home_00${homeButtonImgIndex}.png`
+
+homeButton.addEventListener("mouseover", function (event) {
+	homeButtonInterval = setInterval(() => {
+		homeButtonImgIndex++
+
+		if (homeButtonImgIndex === 8) {
+			homeButtonImgIndex = 1
+		}
+
+		homeButton.src = `../../assets/home_icons/home_00${homeButtonImgIndex}.png`
+	}, 200)
+}, false);
+
+homeButton.addEventListener("mouseleave", function (event) {
+	clearInterval(homeButtonInterval)
+}, false);
